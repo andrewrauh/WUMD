0ea9d586cffc5850a39e275c29b57c344715874e
Andrew Rauh <andrewrauh@Andrew-Rauhs-MacBook-Pro.local>
2011-08-17 16:04:02 -0400

    setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wumd::Application.config.secret_token = '1acef2ad3f44567abbce90b63bfaba17b5ec7bad9873a1ef0dbb0083fa40fbbb6230b95addbbe3dab5c43a39a28606455e3c6b6127390a28448542c0e5d43b1a'
